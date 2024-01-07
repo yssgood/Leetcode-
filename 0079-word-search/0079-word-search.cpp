2fb4a21197d973ae3887b5b654abf884968e4fe8
유승선 <yssgood@naver.com>
2024-01-07 20:44:25 +0900

    Time: 268 ms (90.81%), Space: 8.5 MB (27.09%) - LeetHub

---
+++ 0079-word-search/0079-word-search.cpp
@@ -0,0 +1,29 @@
+class Solution {
+
+public: 
+    bool dfs(vector<vector<char>>& board, string& word, int i, int j, int index){
+        if(index >= word.length()) return true; 
+        if(i < 0 || j < 0 || i >= board.size() || j >= board[0].size() || board[i][j] != word[index]) return false; 
+        char tmp = word[index]; 
+        board[i][j] = '-'; 
+
+        bool flag = (dfs(board,word,i+1,j,index+1) || dfs(board,word,i-1,j,index+1) || 
+                    dfs(board,word,i,j+1,index + 1) || dfs(board,word,i,j-1,index+1));
+
+        
+        board[i][j] = tmp; 
+
+        return flag; 
+    }
+public:
+    bool exist(vector<vector<char>>& board, string word) {
+        for(int i = 0; i < board.size(); i++){
+            for(int j = 0; j < board[i].size(); j++){
+                if(board[i][j] == word[0] && dfs(board,word,i,j,0)){
+                    return true; 
+                }
+            }
+        }
+        return false; 
+    }
+};
