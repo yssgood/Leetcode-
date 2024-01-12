10f0c103546314a638ee90a222650d4d0b076bae
유승선 <yssgood@naver.com>
2024-01-12 22:33:05 +0900

    Time: 2 ms (64.47%), Space: 8.1 MB (64.75%) - LeetHub

---
+++ 0046-permutations/0046-permutations.cpp
@@ -0,0 +1,28 @@
+class Solution {
+
+    void dfs(vector<vector<int>>& answer, vector<int>& container,
+             vector<bool>& visited, vector<int>& nums) {
+        if (container.size() >= nums.size()) {
+            answer.push_back(container);
+            return;
+        }
+        for (int i = 0; i < nums.size(); i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                container.push_back(nums[i]);
+                dfs(answer, container, visited, nums);
+                visited[i] = false;
+                container.pop_back();
+            }
+        }
+    }
+
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> answer;
+        vector<int> container;
+        vector<bool> visited(nums.size(), false);
+        dfs(answer, container, visited, nums);
+        return answer;
+    }
+};
